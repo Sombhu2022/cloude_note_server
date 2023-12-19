cc3b30a0eb38a7bc5bbcfbddc76c75e75c9e134f
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2023-12-19 16:09:56 +0530

    Add files via upload

---
+++ controller/noteControler.js
@@ -0,0 +1,86 @@
+import { Notes } from "../models/noteModel.js";
+
+
+export const getNotes = async(req,res)=>{
+    try {
+        const note = await Notes.find({})
+        
+        res.status(200).json({
+        success: true,
+        note
+      });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error
+          });
+    }
+}
+export const getNote = async(req,res)=>{
+    try {
+        const {id}=req.params
+        const note = await Notes.findById({id})
+        
+        res.status(200).json({
+        success: true,
+        note
+      });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error
+          });
+    }
+}
+export const updateNote = async(req,res)=>{
+    try {
+        const {id}=req.params
+        const data = req.body
+        const note = await Notes.findByIdAndUpdate({id} ,data , {new:true} )
+        
+        res.status(200).json({
+        success: true,
+        note
+      });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error
+          });
+    }
+}
+export const addNote = async (req, res) => {
+  try {
+    const { note } = req.body;
+    console.log(note);
+    await Notes.create({
+      note,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "add note",
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: "Can't add note",
+    });
+  }
+};
+
+export const deletNote= async (req,res)=>{
+    try {
+        const {id} = req.params
+        await Notes.findByIdAndDelete({id})
+        res.status(200).json({
+            success: true,
+            message: "delete note",
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "somthing else , try again",
+        })
+    }
+}
